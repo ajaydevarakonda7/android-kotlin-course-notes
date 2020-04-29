96c7b03e85e50569c07bfcc41d8030b9ed76f80f
Ajay Devarakonda <contact@ajaydevarakonda.dev>
2020-04-29 14:04:21 +0530

    End of Lesson 2 Project - 1.

--- Lesson 2/MyBio/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Lesson 2/MyBio/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName = MyName(name="Ajay Devarakonda")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        // sets content view
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        binding.done.setOnClickListener {
+            addNick(it)
+        }
+    }
+
+    fun addNick(view: View) {
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+
+            nicknameEdit.visibility = View.GONE
+            done.visibility = View.GONE
+
+            nicknameText.visibility = View.VISIBLE
+        }
     }
 }
