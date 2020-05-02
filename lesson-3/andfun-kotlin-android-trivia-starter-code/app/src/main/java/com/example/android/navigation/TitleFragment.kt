7b00c10f23ae2d7bdf9779fe117c68015e0dc208
Ajay Devarakonda <contact@ajaydevarakonda.dev>
2020-05-02 09:36:17 +0530

    Add android navigation bar up button.

--- lesson-3/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ lesson-3/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -14,9 +14,20 @@ import com.example.android.navigation.databinding.FragmentTitleBinding
  */
 class TitleFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater, R.layout
+                .fragment_title, container, false
+        )
+
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener
+                (R.id.action_titleFragment_to_gameFragment)
+        )
+
         return binding.root
     }
 
